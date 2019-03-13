8678bb867b00648a0e3cb04bcf38513b05d4eb51
Alex lechot <admin@uxmilk.ch>
2019-03-13 15:44:34 +0100

    Header done

--- apaxyApp/src/components/Header/Header.js
+++ apaxyApp/src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import Style from './Header.module.css'
+import Style from './Header.module.css';
+import logo from '../../images/logo/apaxy-v2.svg';
 
 class Header extends Component {
     render() {
         return(
-            <div className={Style.container}>
-                <h1>Hello</h1>
+            <div>
+                <div className={Style.container}></div>
+                <img className="mx-auto d-block my-5" src={logo} alt="Logo"/>
             </div>
         )
     }
