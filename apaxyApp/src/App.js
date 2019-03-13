2c5d72c7c422ef133fd8ed83477a3c9673c08f70
Alex lechot <admin@uxmilk.ch>
2019-03-13 14:40:33 +0100

    Delete test (Bruno explication)

--- apaxyApp/src/App.js
+++ apaxyApp/src/App.js
@@ -1,31 +1,19 @@
 import React, { Component } from 'react';
-import Header from './components/Header/Header'
+import { Header } from './components';
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      name: null,
-      age: 30
     }
-
-    setTimeout(() =>{
-      this.setState({
-        name: 'bruno'
-      })
-    }, 6000)
   }
 
   render () {
-    const {name, age} = this.state
-
-
     return (
-    <div>
-      <Header name={name} age={age}/>
-      
-    </div>
-  )
+        <div>
+          <Header></Header>
+        </div>
+    )
   }
 }
 export default App
--- apaxyApp/src/components/Header/Header.js
+++ apaxyApp/src/components/Header/Header.js
@@ -3,11 +3,9 @@ import Style from './Header.module.css'
 
 class Header extends Component {
     render() {
-        const {name, age } = this.props
-
         return(
             <div className={Style.container}>
-                <h1 >Hello {name} {age}</h1>
+                <h1>Hello</h1>
             </div>
         )
     }
