0c834ecfc028e8ebf485447a25fbd8538f75f3a9
Alex lechot <admin@uxmilk.co>
2019-03-17 23:03:53 +0100

    Navbar update

--- apaxyApp/src/App.js
+++ apaxyApp/src/App.js
@@ -24,38 +24,38 @@ class App extends Component {
         {/* like index.js - default page that are implemented in all pages */}
 
           <Router>
-          <nav className="navbar navbar-expand navbar-dark bg-dark flex-column flex-md-row bd-navbar fixed-top" >
-              <div className="container">
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                  <NavLink className="navbar-brand" to="/">
-                  <img className="logo" src={logo}/>
-                  </NavLink>
-                  <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li class="nav-item">
-                      <NavLink className="nav-link" to="/icon">Icon</NavLink>
-                    </li>
-                    <li class="nav-item">
-                      <NavLink className="nav-link" to="/doc">Doc</NavLink>
-                    </li>
-                  </ul>
-                  <ul className="my-auto">
-                    <li class="nav-item  d-inline-block">
-                      <a className="nav-link" href="https://hub.docker.com/r/fusengine/apaxy" target="_blank">
-                        <img className="dev" src={docker}/>
-                      </a>
-                    </li>
-                    <li class="nav-item d-inline-block">
-                      <a className="nav-link" href="https://github.com/fusengine/apaxy-v2" target="_blank">
-                        <img className="dev" src={github}/>
-                      </a>
-                    </li>
-                  </ul>
-              </div>  
-            </div>
-          </nav>
-            {/* <NavLink to="/test">test</NavLink> */}
+            <nav className="navbar navbar-expand navbar-dark bg-dark flex-column flex-md-row bd-navbar fixed-top mb-5" >
+                <div className="container">
+                  <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <NavLink className="navbar-brand" to="/">
+                    <img className="logo" src={logo}/>
+                    </NavLink>
+                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                      <li class="nav-item">
+                        <NavLink className="nav-link" to="/icon">Icon</NavLink>
+                      </li>
+                      <li class="nav-item">
+                        <NavLink className="nav-link" to="/doc">Doc</NavLink>
+                      </li>
+                    </ul>
+                    <ul className="my-auto">
+                      <li class="nav-item  d-inline-block">
+                        <a className="nav-link" href="https://hub.docker.com/r/fusengine/apaxy" target="_blank">
+                          <img className="dev" src={docker}/>
+                        </a>
+                      </li>
+                      <li class="nav-item d-inline-block">
+                        <a className="nav-link" href="https://github.com/fusengine/apaxy-v2" target="_blank">
+                          <img className="dev" src={github}/>
+                        </a>
+                      </li>
+                    </ul>
+                </div>  
+              </div>
+            </nav>
+              {/* <NavLink to="/test">test</NavLink> */}
 
-
+            <div className="margintop"></div>
 
             <Switch>
               <Route exact path="/" component={Apaxy}></Route>
--- apaxyApp/src/pages/Apaxy.js
+++ apaxyApp/src/pages/Apaxy.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 
+/** Images */
+import Logo2 from '../images/logo/apaxy-v2-2.svg'
+
 class Apaxy extends Component {
     render() {
         return(
             <div>
 
-            
+            <img className="logo2 mx-auto d-block" src={Logo2}/>
+            <h7>Apaxy v2 is a beautiful</h7>            
 
 
 
