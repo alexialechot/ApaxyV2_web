18d6d8f48d45eabae0a1b5e660a846391302ad0f
Alex lechot <admin@uxmilk.co>
2019-03-17 22:00:40 +0100

    Add animations

--- apaxyApp/src/pages/Apaxy.js
+++ apaxyApp/src/pages/Apaxy.js
@@ -3,7 +3,13 @@ import React, {Component} from 'react';
 class Apaxy extends Component {
     render() {
         return(
-            <h1> Voici ma Home page</h1>
+            <div>
+
+            
+
+
+
+            </div>
         )
     }
 }
