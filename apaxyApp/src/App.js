d628d4e8da69149e7e0e75aa97d767bc0d839fef
Alex lechot <admin@uxmilk.co>
2019-03-18 00:01:13 +0100

    Add <header> and comment

--- apaxyApp/src/App.js
+++ apaxyApp/src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom'
 
 
 /** Component */
-import { Header, Footer } from './components';
+import {Footer } from './components';
 
 /** Image */
 import logo from './images/logo/apaxy-v2.svg';
@@ -20,43 +20,51 @@ class App extends Component {
 
   render () {
     return (
+        
         <Fragment>
         {/* like index.js - default page that are implemented in all pages */}
-
+        
           <Router>
-            <nav className="navbar navbar-expand navbar-dark bg-dark flex-column flex-md-row bd-navbar fixed-top mb-5" >
-                <div className="container">
-                  <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <NavLink className="navbar-brand" to="/">
-                    <img className="logo" src={logo}/>
-                    </NavLink>
-                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                      <li class="nav-item">
-                        <NavLink className="nav-link" to="/icon">Icon</NavLink>
-                      </li>
-                      <li class="nav-item">
-                        <NavLink className="nav-link" to="/doc">Doc</NavLink>
-                      </li>
-                    </ul>
-                    <ul className="my-auto">
-                      <li class="nav-item  d-inline-block">
-                        <a className="nav-link" href="https://hub.docker.com/r/fusengine/apaxy" target="_blank">
-                          <img className="dev" src={docker}/>
-                        </a>
-                      </li>
-                      <li class="nav-item d-inline-block">
-                        <a className="nav-link" href="https://github.com/fusengine/apaxy-v2" target="_blank">
-                          <img className="dev" src={github}/>
-                        </a>
-                      </li>
-                    </ul>
-                </div>  
-              </div>
-            </nav>
-              {/* <NavLink to="/test">test</NavLink> */}
+            <header>
+              <nav className="navbar navbar-expand navbar-dark bg-dark flex-column flex-md-row bd-navbar fixed-top mb-5" >
+                  <div className="container">
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                      <NavLink className="navbar-brand" to="/">
+                        <img className="logo" src={logo}/>
+                      </NavLink>
+                      <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item">
+                          <NavLink className="nav-link" to="/icon">Icon</NavLink>
+                        </li>
+                        <li class="nav-item">
+                          <NavLink className="nav-link" to="/doc">Doc</NavLink>
+                        </li>
+                      </ul>
+                      <ul className="my-auto">
+                        <li class="nav-item  d-inline-block">
+                          <a className="nav-link" href="https://hub.docker.com/r/fusengine/apaxy" target="_blank">
+                            <img className="dev" src={docker}/>
+                          </a>
+                        </li>
+                        <li class="nav-item d-inline-block">
+                          <a className="nav-link" href="https://github.com/fusengine/apaxy-v2" target="_blank">
+                            <img className="dev" src={github}/>
+                          </a>
+                        </li>
+                      </ul>
+                  </div>  
+                </div>
+              </nav>
+            </header>
 
+            {/* Margin top because of the navbar */}
             <div className="margintop"></div>
 
+
+            {/* Here come the containt of the page */}
+
+
+            {/* Select page for each path */}
             <Switch>
               <Route exact path="/" component={Apaxy}></Route>
               <Route path="/icon" component={Icon}></Route>
@@ -67,8 +75,10 @@ class App extends Component {
             </Switch>
           </Router>
         
+
           <Footer></Footer>
         </Fragment>
+        
     )
   }
 }
