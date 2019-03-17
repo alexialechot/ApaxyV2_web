6e7ef466bd2646aac6514a80bc5477d6114e6bda
Alex lechot <admin@uxmilk.co>
2019-03-18 00:02:53 +0100

    Add by uxmilk

--- apaxyApp/src/components/Footer/Footer.js
+++ apaxyApp/src/components/Footer/Footer.js
@@ -5,6 +5,7 @@ class Footer extends Component {
         return(
             <div>
                 <h5 className="text-center my-5">Apaxy v2 by <a href="https://fusengine.ch" target="_blank">Fusengine</a></h5>
+                <h5 className="text-center ">Website by <a href="https://uxmilk.co" target="_blank">uxmilk</a></h5>
                 <div className="pink"></div>
             </div>
         )
