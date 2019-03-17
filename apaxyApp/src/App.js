e2aa8bcb0ebdfcc0188cc20ee537661baed3f1b5
Alex lechot <admin@uxmilk.co>
2019-03-17 21:58:32 +0100

    Navbar & footer

--- apaxyApp/src/App.js
+++ apaxyApp/src/App.js
@@ -1,39 +1,74 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom'
+
 
 /** Component */
 import { Header, Footer } from './components';
 
+/** Image */
+import logo from './images/logo/apaxy-v2.svg';
+import github from './images/fontawesome/github.svg'
+import docker from './images/fontawesome/docker.svg'
+
 /** Pages */
-import { Apaxy, Icon, Doc, Docker } from './pages'
+import { Apaxy, Icon, Doc } from './pages'
+
+
 
 class App extends Component {
 
 
   render () {
     return (
-        <div>
-        {/* like index.js - default page */}
-        <Header></Header>
+        <Fragment>
+        {/* like index.js - default page that are implemented in all pages */}
+
           <Router>
-            <NavLink to="/">Apaxy V2</NavLink>
-            <NavLink to="/icon">Icon</NavLink>
-            <NavLink to="/doc">Doc</NavLink>
-            <NavLink to="/docker">Docker</NavLink>
-            <NavLink to="https://github.com/fusengine/apaxy-v2" target="_blank">Github Repo</NavLink>
+          <nav className="navbar navbar-expand navbar-dark bg-dark flex-column flex-md-row bd-navbar fixed-top" >
+              <div className="container">
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                  <NavLink className="navbar-brand" to="/">
+                  <img className="logo" src={logo}/>
+                  </NavLink>
+                  <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                      <NavLink className="nav-link" to="/icon">Icon</NavLink>
+                    </li>
+                    <li class="nav-item">
+                      <NavLink className="nav-link" to="/doc">Doc</NavLink>
+                    </li>
+                  </ul>
+                  <ul className="mr-sm-2  mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item  d-inline-block mx-auto">
+                      <a className="nav-link" href="https://hub.docker.com/r/fusengine/apaxy" target="_blank">
+                        <img className="dev" src={docker}/>
+                      </a>
+                    </li>
+                    <li class="nav-item d-inline-block">
+                      <a className="nav-link" href="https://github.com/fusengine/apaxy-v2" target="_blank">
+                        <img className="dev" src={github}/>
+                      </a>
+                    </li>
+                  </ul>
+              </div>  
+            </div>
+          </nav>
             {/* <NavLink to="/test">test</NavLink> */}
+
+
+
             <Switch>
               <Route exact path="/" component={Apaxy}></Route>
               <Route path="/icon" component={Icon}></Route>
               <Route path="/doc" component={Doc}></Route>
-              <Route path="/docker" component={Docker}></Route>
+              <Route path="https://hub.docker.com/r/fusengine/apaxy"></Route>
               <Route path="https://github.com/fusengine/apaxy-v2"></Route>
               {/* <Route path="/test" render={()=> { return(<h1>Test</h1>)}}></Route> */}
             </Switch>
           </Router>
         
           <Footer></Footer>
-        </div>
+        </Fragment>
     )
   }
 }
--- apaxyApp/src/components/Footer/Footer.js
+++ apaxyApp/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
     render() {
         return(
             <div>
-                <h5 className="text-center my-5">Apaxy v2 by Fusengine</h5>
+                <h5 className="text-center my-5">Apaxy v2 by <a href="https://fusengine.ch" target="_blank">Fusengine</a></h5>
                 <div className="pink"></div>
             </div>
         )
