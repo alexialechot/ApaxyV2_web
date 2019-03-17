69ac82a3c88ee57397f5819188fee1ea050eec18
Alex lechot <admin@uxmilk.co>
2019-03-18 00:02:29 +0100

    Add Screenshot

--- apaxyApp/src/pages/Apaxy.js
+++ apaxyApp/src/pages/Apaxy.js
@@ -2,17 +2,15 @@ import React, {Component} from 'react';
 
 /** Images */
 import Logo2 from '../images/logo/apaxy-v2-2.svg'
+import Screenshot from '../images/screenshot/ScreenshotApaxy.png'
 
 class Apaxy extends Component {
     render() {
         return(
-            <div>
-
-            <img className="logo2 mx-auto d-block" src={Logo2}/>
-            <h7>Apaxy v2 is a beautiful</h7>            
-
-
-
+            <div className="container">
+                <img className="logo2 mx-auto d-block" src={Logo2}/>
+                <h6 className="text-center">Apaxy v2 is a beautiful interface Apache 2</h6>            
+                <img className="screenshot mx-auto d-block" src={Screenshot}/>
             </div>
         )
     }
