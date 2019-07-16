f33e7747adfd2e39a114d4dbafb187b98fd35fac
Alexia <alexia@krakenwave.ch>
2019-07-16 15:49:37 +0200

    Add img

--- apaxywebapp/src/img/index.js
+++ apaxywebapp/src/img/index.js
@@ -1,2 +1,4 @@
 /** Logo */
-export { default as logo } from './logo.svg'
+export { default as logo } from './logo/apaxy-v2.svg'
+export { default as logosquare } from './logo/apaxy-v2-square.svg'
+
